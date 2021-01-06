01873022e202ca5a0adc6bfee07ad49a87c13aa5
Debanik Dawn <debanik96@gmail.com>
2021-01-07 00:16:43 +0530

    Add test helpers and test for db metadata

---
+++ tests/models/helpers.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Database from "../../src/models/database";
+import Graph from "../../src/models/graph";
+import Node from "../../src/models/node";
+import Relationship from "../../src/models/relationship";
+
+export function testRelationship({
+  id = 0,
+  leftNodeId = 0,
+  rightNodeId = 0,
+}): Relationship {
+  const leftNode = new Node(leftNodeId);
+  const rightNode = new Node(rightNodeId);
+
+  return new Relationship(id, leftNode, rightNode);
+}
+
+export function testNode({ id = 0, relationships = [], data = {} }): Node {
+  if (relationships.length === 0) {
+    const dummyRelationship = testRelationship({
+      leftNodeId: id,
+      rightNodeId: id,
+    });
+    return new Node(id, [dummyRelationship], data);
+  }
+
+  return new Node(id, relationships, data);
+}
+
+export function testGraph({
+  id = 0,
+  name = "test",
+  nodes = [] as Array<Node>,
+}): Graph {
+  if (nodes.length === 0) {
+    const dummyNode = testNode({});
+    return new Graph(id, name, [dummyNode]);
+  }
+
+  return new Graph(id, name, nodes);
+}
+export function testDatabase({
+  id = 0,
+  name = "test",
+  graphs = [] as Array<Graph>,
+}): Database {
+  if (graphs.length === 0) {
+    const dummyGraph = testGraph({});
+    return new Database(id, name, [dummyGraph]);
+  }
+
+  return new Database(id, name, graphs);
+}
