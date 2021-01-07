5fa419e8fcbb6e381ab0070b8754f2ea7ae6c003
Debanik Dawn <debanik96@gmail.com>
2021-01-07 19:30:53 +0530

    Add coverage reporter and threshold

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,13 @@ module.exports = {
     "^.+\\.(js|jsx)$": "babel-jest",
   },
   coveragePathIgnorePatterns: ["tests"],
+  coverageReporters: ["html"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
