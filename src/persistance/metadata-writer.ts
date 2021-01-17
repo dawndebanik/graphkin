e286cd5fa35f4c190b4f83741e79585c8812a13b
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2021-01-17 11:52:58 +0530

    Removed the redundant check for metadata type.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

--- src/persistance/metadata-writer.ts
+++ src/persistance/metadata-writer.ts
@@ -8,20 +8,17 @@ export default class MetadataWriter {
   writeMetadata(metaData: Metadata): void {
     const metadataRootDir = process.env[META_DATA_PATH_KEY];
 
-    if (metaData.type) {
-      const fileName = `${metaData.type}.${DATA_FILE_EXTENSION}`;
-      const fileLocation = `${metadataRootDir}/${metaData.modelName}/types/${fileName}`;
-
-      let typeData: number[] = [];
-      try {
-        fs.accessSync(fileLocation, fs.constants.W_OK | fs.constants.R_OK);
-        typeData = JSON.parse(fs.readFileSync(fileLocation, "utf-8"));
-      } catch (e) {}
-
-      typeData.push(metaData.id);
-      _writeDataToFile(fileLocation, JSON.stringify(typeData));
+    const typeFileName = `${metaData.type}.${DATA_FILE_EXTENSION}`;
+    const typeFileLocation = `${metadataRootDir}/${metaData.modelName}/types/${typeFileName}`;
+    let typeData: number[] = [];
+    try {
+      fs.accessSync(typeFileName, fs.constants.W_OK | fs.constants.R_OK);
+      typeData = JSON.parse(fs.readFileSync(typeFileLocation, "utf-8"));
+    } catch (e) {
+      //File doesn't exists.
     }
-
+    typeData.push(metaData.id);
+    _writeDataToFile(typeFileLocation, JSON.stringify(typeData));
     const fileName = `${metaData.id}.${DATA_FILE_EXTENSION}`;
     const fileLocation = `${metadataRootDir}/${metaData.modelName}/${fileName}`;
     _writeDataToFile(fileLocation, JSON.stringify(metaData.data));
